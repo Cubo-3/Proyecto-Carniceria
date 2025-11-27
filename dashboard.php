ea0ada2dab4f4de69aa39d986b5204253973540d
Cubo-3 <andrescastrop124@gmail.com>
2025-11-27 18:38:52 -0500

    cambios

--- dashboard.php
+++ dashboard.php
@@ -56,7 +56,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Carnicería</title>
+    <title>Dashboard - Carnicería-La Fama</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Iconos de Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
@@ -123,7 +123,7 @@ try {
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
         <div class="container">
-            <a class="navbar-brand" href="#">Carnicería</a>
+            <a class="navbar-brand" href="#">Carnicería-La Fama</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
